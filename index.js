f9f7404157f1e1656b170485e6f2f984c1452529
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2024-04-26 22:18:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-src="https://kit.fontawesome.com/yourkitid.js"
-crossorigin="anonymous"
->
-
 var buttons = document.querySelectorAll("button");
 
 for (var i = 0; i < buttons.length; i++) {
@@ -9,3 +5,34 @@ for (var i = 0; i < buttons.length; i++) {
     this.nextElementSibling.classList.toggle("show");
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var descriptions = document.querySelectorAll(".description");
+  descriptions.forEach(function (description) {
+    description.style.display = "none";
+  });
+
+  var buttons = document.querySelectorAll("button");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var description = this.nextElementSibling;
+      if (description.style.display === "none") {
+        description.style.display = "block";
+      } else {
+        description.style.display = "none";
+      }
+    });
+  });
+});
+
+/////
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll("button");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var description = this.nextElementSibling;
+      description.classList.toggle("active");
+    });
+  });
+});
